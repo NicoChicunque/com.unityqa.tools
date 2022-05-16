0b460e8a1ff628d44ba07e6f1c66c3c3ca55f1d2
Nicolas Chicunque <nico.chicunque@unity3d.com>
2022-05-16 06:44:27 -0500

    Logger.cs

--- Editor/Logger.cs
+++ Editor/Logger.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.IO;
 using UnityEditor;
-//using System.Threading.Tasks;
 using UnityEngine;
+using System.Threading.Tasks;
 
 namespace UnityQA.Tools
 {
@@ -12,7 +12,6 @@ namespace UnityQA.Tools
         public static void PrintInfo()
         {
             Application.logMessageReceivedThreaded += LogMessageReceivedThreaded;
-            
             Debug.Log("Hello");
             Debug.Log("World");
             Debug.Log("QA");
@@ -23,17 +22,18 @@ namespace UnityQA.Tools
         private static void LogMessageReceivedThreaded(string logString, string stackTrace, LogType type)
         {
             string path = "./LogsQA/Log" + DateTime.Now.ToString("yyyyMMddHHmm") + ".txt";
-            //SaveToFile(logString, "./Logs/Log" + DateTime.Now.ToString("yyyy/MM/dd/HH:mm") + ".txt");
-            StreamWriter log = File.Exists(path) ? File.AppendText(path) : new StreamWriter(path);
-            /*await*/log.WriteLineAsync(logString);
+            //const string path = "./artifacts/SavingLog.txt";
+
+            SaveToFile(logString, path);
+        }
+
+        private static async Task SaveToFile(string stringToSave, string path) // Changed Task to void: https://stackoverflow.com/questions/14903887/warning-this-call-is-not-awaited-execution-of-the-current-method-continues
+        {
+            StreamWriter log = !File.Exists(path) ? new StreamWriter(path) : File.AppendText(path);
+
+            await log.WriteLineAsync(stringToSave);
+
             log.Close();
         }
-        
-        //private static async Task SaveToFile(string stringToSave, string path) // Changed Task to void: https://stackoverflow.com/questions/14903887/warning-this-call-is-not-awaited-execution-of-the-current-method-continues
-        //{
-        //    StreamWriter log = !File.Exists(path) ? new StreamWriter(path) : File.AppendText(path);            
-        //    await log.WriteLineAsync(stringToSave);
-        //    log.Close();
-        //}
     }
 }
